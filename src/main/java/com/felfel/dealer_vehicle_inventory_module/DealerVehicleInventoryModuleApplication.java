4e2fe279746b9bb358855fe3ad88c2b6858ef665
Ahmedfelfel <68282845+Ahmedfelfel@users.noreply.github.com>
2026-03-24 18:19:50 +0200

    first commit

---
+++ src/main/java/com/felfel/dealer_vehicle_inventory_module/DealerVehicleInventoryModuleApplication.java
@@ -0,0 +1,13 @@
+package com.felfel.dealer_vehicle_inventory_module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealerVehicleInventoryModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DealerVehicleInventoryModuleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/felfel/dealer_vehicle_inventory_module/DealerVehicleInventoryModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felfel.dealer_vehicle_inventory_module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DealerVehicleInventoryModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
